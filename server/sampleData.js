7f1c8d44540082870f38cd9aaa2815d52f0dcac4
saharsh <saharshdc@gmail.com>
2023-01-02 12:03:40 -0500

    added graphql

---
+++ server/sampleData.js
@@ -0,0 +1,59 @@
+const projects = [
+    {
+        id: '1',
+        clientId: '1',
+        name : 'eCommerce Website',
+        description : 'A website for the marketing and sales of a variety of products',
+        status : 'In Progress'
+    },
+    {
+        id: '2',
+        clientId: '2',
+        name : 'Photo Sharing App',
+        description : "A website to show a photographer's work",
+        status : 'In Progress'
+
+    },
+    {
+        id: '3',
+        clientId: '3',
+        name : 'Dating App',
+        description : 'An app to find and connect with like-minded individuals',
+        status : 'Done'
+
+    },
+    {
+        id: '4',
+        clientId: '4',
+        name : 'Wedding Planner',
+        description : 'A website for couples to find and enlist vedors for their big day',
+        status : 'In Progress'
+    },
+]
+
+const clients = [
+    {
+        id: '1',
+        name : 'Marcus Tanner',
+        email: 'marcus@mtanner.com',
+        phone: '555-433-5234'
+    },
+    {
+        id: '2',
+        name : 'Evenlina Linn',
+        email: 'elinn@gmail.com',
+        phone: '555-412-1224'
+    },
+    {
+        id: '3',
+        name : 'Grimaldi Veayn',
+        email: 'info@gmald.com',
+        phone: '555-228-0987'
+    },
+    {
+        id: '4',
+        name : 'Rajan Kanth',
+        email: 'rajan@kanthdo.com',
+        phone: '555-362-5234'
+    }
+]
